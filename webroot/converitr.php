7585e9b000a6c5b0272ebb5ec24b372c880456bd
tmtrbocanegra <tmtrbocanegra@SISPROG62.TMT.LOCAL>
2016-03-11 10:43:30 -0500

    Intentando hacer rutas

--- webroot/converitr.php
+++ webroot/converitr.php
@@ -11,7 +11,8 @@ if (mysqli_select_db($link, "bd_janytours")) {
             $nuevo_ubigeo["parent_id"] = null;
         } elseif($row["cod_dist"] == $nulo) {
             $nuevo_ubigeo["categoria"] = "Provincia";
-            $nuevo_ubigeo["parent_id"] = $row["cod_dpto"];
+            $nuevo_ubigeo["parent_id"] = found_departamento($row["cod_dpto"], $link);
+            // $nuevo_ubigeo["parent_id"] = $row["cod_dpto"];
         } else {
             $nuevo_ubigeo["categoria"] = "Distrito";
             $nuevo_ubigeo["parent_id"] = $row["cod_prov"];
@@ -20,4 +21,11 @@ if (mysqli_select_db($link, "bd_janytours")) {
         
         var_dump($nuevo_ubigeo);
     }
+}
+
+function found_departamento($cod_dpto, $link) {
+    $query = "SELECT * FROM ubigeos WHERE cod_prov = '00' AND cod_dist = '00' AND cod_dpto = '$cod_dpto'";
+    $result = mysqli_query($link, $query);
+    $row = mysqli_fetch_assoc($result);
+    return $row["id"];
 }
